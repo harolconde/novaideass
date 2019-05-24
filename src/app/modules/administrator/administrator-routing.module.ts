93ffc43ade2870fa336759461231d2f6c84fac16
harolconde <harolconde@gmail.com>
2019-05-24 18:01:53 -0500

    Update perfil administrator

--- src/app/modules/administrator/administrator-routing.module.ts
+++ src/app/modules/administrator/administrator-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdministratorComponent } from './administrator.component'
 import { PerfiladministratorComponent } from './perfiladministrator/perfiladministrator.component'
+import { AllUsersComponent } from './all-users/all-users.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'perfil-administrador',
         component: PerfiladministratorComponent
+      },
+      {
+        path: 'administracion-usuarios',
+        component: AllUsersComponent
       }
     ]
     
--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdministratorRoutingModule } from './administrator-routing.module';
 import { AdministratorComponent } from './administrator.component';
 import { PerfiladministratorComponent } from './perfiladministrator/perfiladministrator.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 @NgModule({
   declarations: [
     AdministratorComponent,
-    PerfiladministratorComponent
+    PerfiladministratorComponent,
+    AllUsersComponent
   ],
   imports: [
     CommonModule,
