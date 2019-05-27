4d0102919632d7b454c476d3f0f707fa5f9ce6bb
harolconde <harolconde@gmail.com>
2019-05-27 18:14:39 -0500

    Update all administrator

--- src/app/modules/administrator/all-ideas/all-ideas.component.ts
+++ src/app/modules/administrator/all-ideas/all-ideas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import  * as $ from 'jquery';
+declare var $:any;
 
 @Component({
   selector: 'app-all-ideas',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllIdeasComponent implements OnInit {
 
+  imgIdeasSrc:string ='/assets/img/pics/photo.svg'
+  fileUpload: File = null;
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Subir imagen de perfil del usuario.
+  selectImg(file: FileList) {
+    this.fileUpload = file.item(0);
+
+    // Upload image
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imgIdeasSrc = event.target.result;
+    };
+    reader.readAsDataURL(this.fileUpload);
+
+  }
+
 }
+(() => {
+  $('[data-toggle="tooltip"]').tooltip()
+});
